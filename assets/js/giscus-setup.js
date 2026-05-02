d0b3a7e208ad5957d8af6eb3873ce3386300b236
Maksudur26 <maksudur.buet17@gmail.com>
2026-05-02 14:27:31 +0000

    Deploying to gh-pages from @ Maksudur26/Maksudur@429eaf971de511c3305982bdbe644c7995531e84 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "Maksudur26/Maksudur",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
